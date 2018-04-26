90c02261d02ef5027af0494bad32b1f2cb5d4fb6
haley <wh46397@163.com>
2018-04-26 15:36:17 +0800

    添加person类

---
+++ GitLearing/GitLearing/person.h
@@ -0,0 +1,13 @@
+//
+//  person.h
+//  GitLearing
+//
+//  Created by lx on 2018/4/26.
+//  Copyright © 2018年 lx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface person : NSObject
+
+@end
---
+++ GitLearing/GitLearing/person.m
@@ -0,0 +1,13 @@
+//
+//  person.m
+//  GitLearing
+//
+//  Created by lx on 2018/4/26.
+//  Copyright © 2018年 lx. All rights reserved.
+//
+
+#import "person.h"
+
+@implementation person
+
+@end
