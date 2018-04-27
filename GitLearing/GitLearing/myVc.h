54f031960f82e6ab8fe23f5bdf3680d3b4f39920
haley <wh46397@163.com>
2018-04-27 14:49:02 +0800

    no message

---
+++ GitLearing/GitLearing/myVc.h
@@ -0,0 +1,13 @@
+//
+//  myVc.h
+//  GitLearing
+//
+//  Created by lx on 2018/4/27.
+//  Copyright © 2018年 lx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myVc : UIViewController
+
+@end
---
+++ GitLearing/GitLearing/myVc.m
@@ -0,0 +1,34 @@
+//
+//  myVc.m
+//  GitLearing
+//
+//  Created by lx on 2018/4/27.
+//  Copyright © 2018年 lx. All rights reserved.
+//
+
+#import "myVc.h"
+
+@interface myVc ()
+@property(nonatomic,strong)UIView *redView;
+@end
+
+@implementation myVc
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self redView];
+}
+
+-(UIView *)redView{
+    if (_redView == nil) {
+        _redView = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 40, 20)];
+        _redView.backgroundColor = [UIColor redColor];
+    }
+    return _redView;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
