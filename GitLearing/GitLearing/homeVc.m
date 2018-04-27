2655a7177c5eb6304f6c59e00d3f5ae196065219
haley <wh46397@163.com>
2018-04-27 14:20:30 +0800

    添加了homeVc

---
+++ GitLearing/GitLearing/homeVc.m
@@ -0,0 +1,35 @@
+//
+//  homeVc.m
+//  GitLearing
+//
+//  Created by lx on 2018/4/27.
+//  Copyright © 2018年 lx. All rights reserved.
+//
+
+#import "homeVc.h"
+
+@interface homeVc ()
+@property(nonatomic,strong)UIButton *clickBtn;
+@end
+
+@implementation homeVc
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self clickBtn];
+}
+
+-(UIButton *)clickBtn{
+    if (_clickBtn == nil) {
+        _clickBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 40, 20)];
+        _clickBtn.center = self.view.center;
+        [_clickBtn setTitle:@"点我" forState:UIControlStateNormal];
+    }
+    return _clickBtn;
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
